bddd361f8b4c102121f6d5edd6890a8d6b390127
Maksim Moryakov <maksim.moryakov@gmail.com>
2021-05-08 08:37:50 +0300

    Исправил ощибки преревью

--- scripts/script.js
+++ scripts/script.js
@@ -21,7 +21,6 @@ const popupFigureCaption = popupFigure.querySelector('.figure__caption');
 const closeImgButton = document.querySelector('#closeImg');
 
 const template = document.querySelector('#template-card').content;
-    const card = template.querySelector('.element').cloneNode(true);
 
 const initialCards = [
    {
@@ -50,7 +49,7 @@ const initialCards = [
    }
  ];  
 
- //+ Функция добавления карточек при загрузке страницы
+ //Добавления карточек при загрузке страницы
  initialCards.forEach (function (item){
    renderCard(item.link, item.name);
 })
@@ -73,7 +72,7 @@ function createCard(link, name) {
    return card;
 }
 
-// Функция добавления карточек через инпут попапа
+// Добавления карточек через инпут попапа
 function formCardSubmitHandler(evt) {
    evt.preventDefault();
    renderCard(inputAddCardLink.value, inputAddCardName.value);
@@ -81,7 +80,7 @@ function formCardSubmitHandler(evt) {
    closePopup(popupAddCard);
 }
 
-// Функция удаления карточек
+//Удаления картинок
 
 function deleteCard(card) {
    card.querySelector('.element__delete-button').addEventListener('click', function(evt) {
@@ -90,14 +89,14 @@ function deleteCard(card) {
    });
    }
 
- // Функция лайка для карточек
+ // Лайка на картинках
 function handleLikeButton(card) {
    card.querySelector('.element__like').addEventListener('click', function(evt) {
        evt.target.classList.toggle('element__like_active');
    })
 }
 
-// открытие попапа фото карточки
+// Открытие попапа фото карточки
 function openPopupImg(card) {
 card.querySelector('.element__image').addEventListener('click', function(evt) {
        openPopup(popupFigure);
@@ -108,14 +107,14 @@ card.querySelector('.element__image').addEventListener('click', function(evt) {
    });
 }
 
-// открытие окна редактирования профиля
+// Открытие окна редактирования профиля
 function openEditPopup() {
   openPopup(popupEditProfile)
   nameInput.value = profileName.textContent;
   jobInput.value = profileText.textContent;
 }
 
-// изменение данных профиля из инпута попапа
+// Изменение данных профиля 
 function formProfileSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -162,31 +161,3 @@ popupFormCard.addEventListener('submit', formCardSubmitHandler);
 closeImgButton.addEventListener('click', () => {
    closePopup(popupFigure);
 });
-
-
-// // открытие окна редактирования профиля
-// function openEditPopup() {
-// 	openPopup(popupEditProfile)
-// 	profileNameForm.value = profileName.textContent;
-// 	profileJobForm.value = profileText.textContent;
-// }
-
-// // добавления класса у popup и получение данных профиля пользователя
-// function openPopup () {
-//    popupEditProfile.classList.add('popup_opened');
-//    profileNameForm.value = profileName.textContent;
-//    profileJobForm.value = profileJob.textContent;
-// }
-
-// // удаление класса у popup
-// function closePopup () {
-//    popupEditProfile.classList.remove('popup_opened');
-// }
-
-// // сохранения измененных данных из popup
-// function savePopupValue (evt) {
-//    evt.preventDefault();
-//    profileName.textContent = profileNameForm.value;
-//    profileJob.textContent = profileJobForm.value;
-//    closePopup(evt);
-// }
