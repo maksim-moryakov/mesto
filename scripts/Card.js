c58f9a07140547840bc8d84eef5fa0a673a66a5a
maksim.moryakov44@yandex.ru <maksim.moryakov44@yandex.ru>
2021-09-06 17:53:51 +0300

    feat: Создал Card.js

--- scripts/Card.js
+++ scripts/Card.js
@@ -4,3 +4,11 @@ const popupFigure = document.querySelector('.popup_type_image');
 const popupFigureImage = popupFigure.querySelector('.figure__image');
 const popupFigureCaption = popupFigure.querySelector('.figure__caption');
 const closeImgButton = document.querySelector('#closeImg');
+
+class Card {
+    constructor(cardSelector, name, link) {
+        this._cardSelector = cardSelector;
+        this._name = name;
+        this._link = link
+    }
+}    
